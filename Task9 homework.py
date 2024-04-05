1651daa5045cdec6f0fb8c827405b7b3a9d3ca9f
VincentGebirge <vincent.kapin@gmail.com>
2024-04-05 16:49:29 +1000

    writing comments at code

--- Task9 homework.py
+++ Task9 homework.py
@@ -11,10 +11,9 @@ data = pd.DataFrame({'whoAmI': lst})
 data.head()
 
 
-one_hot_style = OrdinalEncoder()
-data["whoAmI_OH_code"] = one_hot_style.fit_transform(data[["whoAmI"]])
+one_hot_style = OrdinalEncoder()    #переменная. которая принимает значение функции, чтобы потом заменить значение в категориях
+data["whoAmI_OH_code"] = one_hot_style.fit_transform(data[["whoAmI"]])  #здесь мы создаем копию объекта, в котором будут записаны двоичные значения в категориях human и robot
 data[["whoAmI", "whoAmI_OH_code"]].head(11)
-
 
 
 print(data)
