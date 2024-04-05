1f2b3b5d234870a2130d97b2d2fd55955208873f
VincentGebirge <vincent.kapin@gmail.com>
2024-04-05 17:00:30 +1000

    checking head function

--- Task9 homework.py
+++ Task9 homework.py
@@ -13,7 +13,7 @@ data.head()
 
 one_hot_style = OrdinalEncoder()    #переменная. которая принимает значение функции, чтобы потом заменить значение в категориях
 data["whoAmI_OH_code"] = one_hot_style.fit_transform(data[["whoAmI"]])  #здесь мы создаем копию объекта, в котором будут записаны двоичные значения в категориях human и robot
-data[["whoAmI", "whoAmI_OH_code"]].head(11)
+data[["whoAmI", "whoAmI_OH_code"]].head(20)
 
 
 print(data)
